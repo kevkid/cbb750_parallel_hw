b4c86da50b52a2e4410139929f6ed73d4a39d1ed
Kevin <kevin.lopez.91@gmail.com>
2017-04-17 13:19:09 -0400

    added some parallel stuff

--- propagating-signal-parallel.py
+++ propagating-signal-parallel.py
@@ -3,45 +3,55 @@ import time
 import multiprocessing
 
 #initialize some multiprocessing stuff
-num_processes = 2
+num_processes = 1
 y = multiprocessing.Array('d', 1000, lock=False)
-new_y = multiprocessing.Array('d', 1000, lock=False)
+new_y = multiprocessing.Array('d', 1000, lock=True)
 dt = multiprocessing.Value('d',0, lock=False)
-def init_process(y_to_share, new_y_to_share):
-	global y, new_y
-	y = y_to_share
-	new_y = new_y_to_share
-
-process_pool = multiprocessing.Pool(
-                   num_processes,
-                   initializer=init_process,
-                   initargs=(y, new_y))
+#def init_process(y_to_share, new_y_to_share):
+#	global y, new_y
+#	y = y_to_share
+#	new_y = new_y_to_share
+#
+#process_pool = multiprocessing.Pool(
+#                   num_processes,
+#                   initializer=init_process,
+#                   initargs=(y, new_y))
 
 
 start = time.time()
 # initial conditions
-y = [0] * 1000
+#y = [0] * 1000
 y[480:520] = [1] * 40
 #check y values
-[e for e in y]
+[e for e in set(y)]
+[e for e in set(new_y)]
 # time-step
 dt.value = 0.01
 
 # our rule for reaction-diffusion
 def advance(y_idx):
-
-    new_y[y_idx] += dt * (20 * (y[y_idx - 1] - 2 * y[y_idx] + y[(y_idx + 1) % n])
+    #print y_idx
+    new_y[y_idx] += dt.value * (20 * (y[y_idx - 1] - 2 * y[y_idx] + y[(y_idx + 1) % len(y)])
 						   - y[y_idx] * (1 - y[y_idx]) * (0.3 - y[y_idx]))
+    #Strange it seems to only update 2... i have no idea why investigating
+    print (dt.value * (20 * (y[y_idx - 1] - 2 * y[y_idx] + y[(y_idx + 1) % len(y)])
+						   - y[y_idx] * (1 - y[y_idx]) * (0.3 - y[y_idx])))
+    #print new_y[y_idx]
+    #return y_idx
 
 # advance through t (t = i * dt) is at least 100; plot
 # every 20
 i = 0
-while i * dt <= 100:
-	if i * dt % 20 == 0:
-		pyplot.plot(y, label='t = %g' % (i * dt))
-	process_pool.map(advance, range(len(y)))
-	i += 1
-	print i * dt
+p = multiprocessing.Pool(num_processes)
+while i * dt.value <= 100:
+    if i * dt.value % 20 == 0:
+        pyplot.plot(y, label='t = %g' % (i * dt.value))
+    p.map(advance, range(len(y)))
+    #p.close()
+    #p.join()
+    y = new_y
+    i += 1
+    #print i * dt.value
 end = time.time()
 
 elapsed = end-start
